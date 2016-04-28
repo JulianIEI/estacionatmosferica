531c20238c5a93b249516916e0c6adfe02e3551b
Julián Fernández Ortiz <jfernandez.iei@gmail.com>
2016-04-28 12:15:17 +0200

    Added files via upload
    Primer caso práctico desarrollado en el proyecto ara familiazarse con el IDE de Arduino y el lenguaje de programación del mismo.

---
+++ Semaforo_con_parpadeo.ino
@@ -0,0 +1,43 @@
+/*
+  Semáforo que hace lo siguiente: Verde + amarillo + rojo + parpadeo en amarillo
+ */
+ 
+// Declaramos los 2 pines digitales y el entero que hará de contador en el bucle del loop
+int ledv = 13;
+int leda = 12;
+int ledr = 11;
+int i=0;
+
+// Definimos los pines como salida (los de los leds)
+void setup() {                
+  // Los iniciamos.
+  pinMode(ledv, OUTPUT);
+  pinMode(leda, OUTPUT);
+  pinMode(ledr, OUTPUT);
+}
+
+// Esto se repite infinitamente
+void loop() {
+  digitalWrite(ledv, HIGH);   // Enciendo verde
+  delay(5000);               // Espero 5s
+  digitalWrite(ledv, LOW);    // Apago verde
+  delay(100);               // Espero 0.1s
+  digitalWrite(leda, HIGH);
+  delay(1000);
+  digitalWrite(leda,LOW);
+  delay(100);
+  digitalWrite(ledr,HIGH);
+  delay(2500);
+  digitalWrite(ledr,LOW);
+  delay(100);
+  for (i=0;i<10;i++){ //Consigamos el parpadeo
+    if(i%2==0){ //En las ocasiones pares lo enciende y se espera 0.5 segundos
+      digitalWrite(leda,HIGH);
+      delay(500);
+    }
+    else{ //en el resto lo apaga y se espera 0.5 segundos. 
+      digitalWrite(leda,LOW);
+      delay(500);
+    }
+  };
+}
